36fc949afe0d99bd65841087cf38dc2e6206fcab
@Kumar <krishnakumar.1905059@srec.ac.in>
2022-03-06 02:19:01 +0530

    Update QN2.java

--- 01-first-java/QN2.java
+++ 01-first-java/QN2.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class QN2 {
     public static void main(String[] args) {
-//        2. 2. Take name as input and print a greeting message for that name.
+//        2. Take name as input and print a greeting message for that name.
         Scanner scanner = new Scanner(System.in);
         String name = scanner.nextLine();
         System.out.println("Good Morning "+name+"\nHava a Nice Day !!!");
